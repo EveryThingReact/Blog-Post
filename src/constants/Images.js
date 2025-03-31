f603a2cef130d7279d30d5d269a134fa6eac046f
egbejob680 <jobegbe39@gmail.com>
2025-03-31 23:38:44 +0100

    Constant files

---
+++ src/constants/Images.js
@@ -0,0 +1,27 @@
+import Logo from '../assets/Logo.svg';
+import HeroImage from '../assets/Welcome.svg';
+import SearchIcon from '../assets/play.svg';
+import post1Image from '../assets/post/pexels-picography-4458.jpg'
+import postProfile from '../assets/post/Ellipse 4.svg'
+import github from '../assets/github.svg'
+import linkin from '../assets/linkedin.svg'
+import CtaImage from '../assets/Profiles.svg'
+
+import x from '../assets/x.svg'
+import instagram from '../assets/instagram.svg'
+
+
+
+const images = {
+    Logo,
+    HeroImage,
+    SearchIcon,
+    post1Image,
+    postProfile,
+    CtaImage,
+    github,
+    linkin,
+    x,
+    instagram,
+};
+export default images
