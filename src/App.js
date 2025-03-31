40e654e6d51ea5505ca4e5ce8e34cc3bb9ecfe67
egbejob680 <jobegbe39@gmail.com>
2025-03-31 23:42:46 +0100

    modified

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Post from './components/Post';
-import Home from './components/Home';
-import Newpost from './components/Newpost';
-import PostDetail from './components/PostDetails';
-import SignupForm from './components/Signup';
-import LoginForm from './components/Login';
-import Navbar from './components/Navbar';
-import ContactUs from './components/ContactUs';
-import AboutUs from './components/AboutUs';
-
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import ArticleDetailPage from './pages/ArticleDetail/ArticleDetailPage';
+import RegisterPage from './pages/register/RegisterPage';
 function App() {
   return (
     <div className="App">
-     <Router>
-         <Routes>
-         <Route path="nav" element={<Navbar />} />
-         <Route path="/contact" element={<ContactUs />} />
-         <Route path="/about" element={<AboutUs />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/post/:id" element={<Post />} />
-          <Route path="/new-post" element={<Newpost/>} />
-          <Route path="/postdetails/:id" element={<PostDetail/>} />
-          <Route path="/signup" element={<SignupForm/>} />
-          <Route path="/login" element={<LoginForm/>} />
-        </Routes>
-     
-    </Router>
+      <Routes>
+        <Route index path='/' element={<HomePage/>}/>
+        <Route path='/blog/:id' element={<ArticleDetailPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
+
+
+      </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      color:{
+        primary: "#1565D8",
+        dark: {
+          light:"#5A7184",
+         hard:"#0D2436",
+         soft:"#183856",
+        },
+      },
+      /*fontFamily:{
+        opensans:["'Open Sans'", "sans-serif"],
+        roboto:["'Roboto'","sans-serif"],
+      }*/
+    },
   },
   plugins: [],
 }
