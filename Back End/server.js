4bcf3cbd70e69e9fcab74753d71920f6c993934b
egbejob680 <jobegbe39@gmail.com>
2025-04-11 13:51:28 +0100

    file modified

--- Back End/server.js
+++ Back End/server.js
@@ -2,13 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/UserRoutes.js";
 import { errorResponseHandler, invalidPathHandler } from "./middleware/errorHandler.js";
+import connectDB from "./config/db.js";
+import cors from 'cors';
 //Routes
 dotenv.config();
 const app = express();
-app.get("/",(req,res) => {
+
+connectDB();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true, // for auth
+  }));
+  app.use(express.json());
+  app.get("/",(req,res) => {
     res.send("Server is running....");
 });
-
 app.use("/api/users", userRoutes);
 app.use(errorResponseHandler);
 app.use(invalidPathHandler);
